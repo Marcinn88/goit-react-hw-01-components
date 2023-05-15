5ce30c78850fde63ff50d6f50969201d465cbc47
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-05-15 23:32:59 +0200

    fx

--- src/index.js
+++ src/index.js
@@ -10,15 +10,16 @@ import data from './data/data.json';
 import friends from './data/friends.json';
 import transactions from './data/transactions.json';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Profile
+    {/* <Profile
       username={user.username}
       tag={user.tag}
       location={user.location}
       avatar={user.avatar}
       stats={user.stats}
-    />
+    /> */}
   <Statistics title="Upload stats" stats={data} />
   <Statistics stats={data} />
   <FriendList friends={friends} />
