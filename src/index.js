5ff48faaf2b23c8ddb5220e568c24e560a5f5603
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-05-15 23:21:53 +0200

    fx

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { Profile } from './components/Profile/Profile';
+import { Profile } from './components/Profile/Profile';
 import { Statistics } from './components/statistics/Statistics';
 import { FriendList } from './components/FriendList/FriendList';
 import {TransactionHistory} from './components/TransactionHistory/TransactionHistory'
 import './index.css';
-// import user from './data/user.json'
+import user from './data/user.json'
 import data from './data/data.json';
 import friends from './data/friends.json';
 import transactions from './data/transactions.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Profile
+    <Profile
       username={user.username}
       tag={user.tag}
       location={user.location}
       avatar={user.avatar}
       stats={user.stats}
-    /> */}
+    />
 
   <Statistics title="Upload stats" stats={data} />
   <Statistics stats={data} />
