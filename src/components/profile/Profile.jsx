839e56647606132ef63385c76664f5e877e7acfb
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-05-16 00:04:25 +0200

    fx

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import PropTypes from "prop-types";
 import css from './Profile.css';
 
@@ -39,14 +39,14 @@ export const Profile =({
   </ul>
 </div>);
 
-// Profile.propTypes = {
-//     username: PropTypes.string.isRequired,
-//     tag: PropTypes.string.isRequired,
-//     location: PropTypes.string.isRequired,
-//     avatar: PropTypes.string.isRequired,
-//     stats: PropTypes.shape({
-//       followers: PropTypes.number.isRequired,
-//       views: PropTypes.number.isRequired,
-//       likes: PropTypes.number.isRequired,
-//     }).isRequired,
-//   };
+Profile.propTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }).isRequired,
+  };
