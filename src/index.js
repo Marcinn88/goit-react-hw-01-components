f5d05ba9bf8e33e2f33a85c01688a86eee190263
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-05-15 23:34:59 +0200

    x

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Statistics } from './components/statistics/Statistics';
 import { FriendList } from './components/FriendList/FriendList';
 import {TransactionHistory} from './components/TransactionHistory/TransactionHistory'
 import './index.css';
-// import user from './data/user.json'
+import user from './data/user.json'
 import data from './data/data.json';
 import friends from './data/friends.json';
 import transactions from './data/transactions.json';
