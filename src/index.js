1123290d47e4f61d40013c994060fd931f017775
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-05-15 23:34:00 +0200

    f

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Profile } from './components/Profile/Profile';
+// import { Profile } from './components/Profile/Profile';
 import { Statistics } from './components/statistics/Statistics';
 import { FriendList } from './components/FriendList/FriendList';
 import {TransactionHistory} from './components/TransactionHistory/TransactionHistory'
 import './index.css';
-import user from './data/user.json'
+// import user from './data/user.json'
 import data from './data/data.json';
 import friends from './data/friends.json';
 import transactions from './data/transactions.json';
