7b0f34f47879007e3db1fb4b76ab7555759f0543
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-05-15 23:57:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Statistics } from './components/Statistics/Statistics';
-import { FriendList } from './components/FriendList/FriendList';
-import { TransactionHistory } from './components/TransactionHistory/TransactionHistory';
-import { Profile } from './components/Profile/Profile';
-import './index.css';
-import user from './data/user.json';
-import data from './data/data.json';
-import friends from './data/friends.json';
-import transactions from './data/transactions.json';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Statistics } from "./components/Statistics/Statistics";
+import { FriendList } from "./components/FriendList/FriendList";
+import { TransactionHistory } from "./components/TransactionHistory/TransactionHistory";
+import { Profile } from "./components/Profile/Profile";
+import "./index.css";
+import user from "./data/user.json";
+import data from "./data/data.json";
+import friends from "./data/friends.json";
+import transactions from "./data/transactions.json";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
