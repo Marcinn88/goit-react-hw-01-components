cecd02325a8c5f6043465a87a1d1ed634741e2b6
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-05-15 21:10:13 +0200

    work

---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+// import PropTypes from "prop-types";
+
+const randomHexColor = () => {Math.floor(Math.random() * 16777215).toString(16)};
+
+export const Statistics = ({ title = 'Upload stats', stats }) => {
+    randomHexColor()
+    return (
+      <section className="statistics">
+        <h2 className="title">{title}</h2>
+        <ul className="stat-list">
+          {stats.map(({ id, label, percentage }) => (
+            <li
+              className="item"
+              key={id}
+              style={{ backgroundColor: randomHexColor() }}
+            >
+              <span className="label">{label}</span>
+              <span className="percentage">{percentage}%</span>
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+// import { App } from 'components/App';
+import { Profile } from './components/profile/Profile';
+import { Statistics } from './components/statistics/Statistics';
 import './index.css';
+import user from './data/user.json'
+import data from './data/data.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+
+  <Statistics title="Upload stats" stats={data} />
+  <Statistics stats={data} />
   </React.StrictMode>
 );
